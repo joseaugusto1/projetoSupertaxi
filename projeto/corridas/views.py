93c587d0ed53946a70dae6a271ce286ccf0e4b28
joseaugusto1 <gutopferreira@msn.com>
2014-11-13 00:22:26 +0000

    alteração2

--- projeto/corridas/views.py
+++ projeto/corridas/views.py
@@ -4,3 +4,19 @@ from corridas.forms import CorridasForm
 def index(request):
     form = CorridasForm()
     return render(request, 'index.html',{'form':form})
+
+def validar(request):
+	if request.method =="POST":
+        form = CorridasForm(request.POST)
+        
+        if form.is_valid():
+            corridas = Corridas(**form.cleaned_data)
+            corridas.save()
+            
+            corridas = Corridas.objects.all()
+            
+            return render(request,'validar.html',{'form':form,'corridas':corridas})
+        
+
+		
+        
